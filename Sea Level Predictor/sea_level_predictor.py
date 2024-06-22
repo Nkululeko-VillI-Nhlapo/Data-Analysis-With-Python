399311f8a1c36d3a4bd2b74344c6efe9d7a45ed3
Nkululeko-VillI-Nhlapo <104261373+Nkululeko-VillI-Nhlapo@users.noreply.github.com>
2024-06-22 19:22:59 +0200

    Add files via upload

---
+++ Sea Level Predictor/sea_level_predictor.py
@@ -0,0 +1,36 @@
+# sea_level_predictor.py
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
+def draw_plot():
+    # Read data from file
+    df = pd.read_csv('epa-sea-level.csv')
+    
+    # Create scatter plot
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
+    
+    # Create first line of best fit
+    slope, intercept, r_value, p_value, std_err = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    years_ext = range(1880, 2051)
+    plt.plot(years_ext, intercept + slope * pd.Series(years_ext), 'r', label='Best fit line 1880-2050')
+    
+    # Create second line of best fit
+    df_recent = df[df['Year'] >= 2000]
+    slope_recent, intercept_recent, r_value_recent, p_value_recent, std_err_recent = linregress(df_recent['Year'], df_recent['CSIRO Adjusted Sea Level'])
+    years_recent_ext = range(2000, 2051)
+    plt.plot(years_recent_ext, intercept_recent + slope_recent * pd.Series(years_recent_ext), 'g', label='Best fit line 2000-2050')
+    
+    # Add labels and title
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
+    plt.xticks(range(1850, 2100, 25))
+    plt.legend()
+    
+    # Save plot and return data for testing (DO NOT MODIFY)
+    plt.savefig('sea_level_plot.png')
+    return plt.gca()
+
